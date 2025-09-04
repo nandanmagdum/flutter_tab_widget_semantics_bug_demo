8afd4e356dd2fcf9b6a7e9f9fa499c792ab5b16b
nandanmagdum14 <nandanmagdum14@gmail.com>
2025-09-04 13:56:26 +0530

    added semantics with role

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,12 +16,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> with TickerProviderStateMixin<MyApp> {
   late TabController _tabController;
   late TabController _tabController2;
-
+  late TabController _tabController3;
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 4, vsync: this);
     _tabController2 = TabController(length: 2, vsync: this);
+    _tabController3 = TabController(length: 4, vsync: this);
   }
 
   @override
@@ -36,22 +39,26 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin<MyApp> {
         appBar: AppBar(title: const Text('TabBar Bug Demo')),
         body: Column(
           children: [
+            // ✅ 1st tabbar
             TabBar(
               controller: _tabController,
               tabs: [
                 Tab(text: 'Tab 0'),
                 Semantics(
                   label: 'label1',
-                  child: Tab(text: 'Tab 1')),
+                  child: Tab(text: 'Tab 1'),
+                ),
                 Semantics(
                   label: 'label2',
                   excludeSemantics: true,
-                  child: Tab(text: 'Tab 2')),
+                  child: Tab(text: 'Tab 2'),
+                ),
                 Semantics(
                   label: 'label3',
                   button: true,
                   enabled: true,
-                  child: Tab(text: 'Tab 3')),
+                  child: Tab(text: 'Tab 3'),
+                ),
               ],
             ),
             Container(
@@ -59,16 +66,19 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin<MyApp> {
               color: Colors.blueGrey.shade100,
               child: TabBarView(
                 controller: _tabController,
-                children:  [
+                children: [
                   myBox(text: 'Pure Tab bar with no semantics tag'),
                   myBox(text: 'Tab with only label'),
                   myBox(text: 'Tab with label and excludeSemantics as true'),
-                  myBox(text: 'Tab with label and button as true and enabled as true'),
+                  myBox(
+                    text:
+                        'Tab with label and button as true and enabled as true',
+                  ),
                 ],
               ),
             ),
 
-            // 2nd tab
+            // ✅ 2nd tabbar
             TabBar(
               controller: _tabController2,
               tabs: [
@@ -76,7 +86,8 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin<MyApp> {
                 MergeSemantics(
                   child: Semantics(
                     label: 'label 5',
-                    child: Tab(text: 'Tab 5')),
+                    child: Tab(text: 'Tab 5'),
+                  ),
                 ),
               ],
             ),
@@ -85,9 +96,46 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin<MyApp> {
               color: Colors.blueGrey.shade100,
               child: TabBarView(
                 controller: _tabController2,
-                children:  [
+                children: [
                   myBox(text: 'This is ExcludeSemantics tab'),
                   myBox(text: 'Tab with only label'),
+                ],
+              ),
+            ),
+
+            // ✅ 3rd tabbar
+            TabBar(
+              controller: _tabController3,
+              tabs: [
+                Semantics(
+                  role: SemanticsRole.tab,
+                  label: 'custom label 1',
+                  child: Tab(text: 'Tab 6'),
+                ),
+                Semantics(
+                  role: SemanticsRole.tabBar,
+                  label: 'custom label 2',
+                  child: Tab(text: 'Tab 7'),
+                ),
+                Semantics(
+                  role: SemanticsRole.tabPanel,
+                  label: 'custom label 3',
+                  child: Tab(text: 'Tab 8'),
+                ),
+                ExcludeSemantics(child: Semantics(
+                  label: 'custom label 4',
+                  child: Tab(text: 'Tab 9'))),
+              ],
+            ),
+            Container(
+              height: 100,
+              color: Colors.blueGrey.shade100,
+              child: TabBarView(
+                controller: _tabController3,
+                children: [
+                  myBox(text: 'Semantics with rold = tab'),
+                  myBox(text: 'Semantics with rold = tabBar'),
+                  myBox(text: 'Semantics with rold = tabPanel'),
                 ],
               ),
             ),
@@ -98,7 +146,6 @@ class _MyAppState extends State<MyApp> with TickerProviderStateMixin<MyApp> {
   }
 }
 
-
-Widget myBox({required String text}){
-  return SizedBox(height: 40,  width: 80,  child: Center(child: Text(text)));
+Widget myBox({required String text}) {
+  return SizedBox(height: 40, width: 80, child: Center(child: Text(text)));
 }
